def5d555dcc4ee1ce74649b8fb926e9a1e7fcb22
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-02-11 13:43:33 +0700

    finalize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import "./App.css";
+import { useState } from "react";
+import CardRating from "~/components/card/CardRating";
+import CardFeedback from "~/components/card/CardFeedback";
 
 function App() {
-  return <div className="App"></div>;
+  const [rating, setRating] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+  };
+
+  console.log(rating);
+
+  return (
+    <>
+      <main className="App">
+        <form onSubmit={handleSubmit}>
+          {isSubmitted && rating ? (
+            <CardFeedback rating={rating} />
+          ) : (
+            <CardRating setRating={setRating} />
+          )}
+        </form>
+      </main>
+      <footer>
+        Challenge by.{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/faizmrtdha">faizmrtdha</a>.
+      </footer>
+    </>
+  );
 }
 
 export default App;
